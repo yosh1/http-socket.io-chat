af689830ee8bc3942ddce7b687c942923c136ec9
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-02-10 21:43:54 +0900

    [Update] let/var -> const

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const http = require("http");
-const socketio = require("socket.io");
-const fs = require("fs");
-const PORT = process.env.PORT || 3000;
+const http = require("http"),
+    socketio = require("socket.io"),
+    fs = require("fs"),
+    PORT = process.env.PORT || 3000;
 
-var server = http.createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
     res.writeHead(200, {
         "Content-Type": "text/html"
     });
-    var output = fs.readFileSync("./index.html", "utf-8");
+    const output = fs.readFileSync("./index.html", "utf-8");
     res.end(output);
     console.log('Port:' + PORT);
 }).listen(PORT);
 
-var io = socketio.listen(server);
+const io = socketio.listen(server);
 
 io.sockets.on("connection", function (socket) {
 
